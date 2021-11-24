35a33bcfb465c4fc05bd4d15899028dea009cd9b
Anstayker <Christian.rojas.bl@gmail.com>
2021-11-24 11:47:33 -0400

    Comentario eliminado

--- Assets/Scripts/PlayerShot.cs
+++ Assets/Scripts/PlayerShot.cs
@@ -12,7 +12,6 @@ public class PlayerShot : MonoBehaviour {
             if (isShooting)
                 gun.Play();
             else
-                //Debug.Log("Me detengo");
                 gun.Stop();
         }
     }

e5032e2cccb5b68813b0eebc97f78688d41b9121
Kevin Aguilar <aguilarkevin825@gmail.com>
2021-11-26 15:40:17 -0400

    Music Background coding standard fixed

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Awake()
-    {
+public class MusicManager : MonoBehaviour {
+    void Awake() {
         MusicManager[] objs = GameObject.FindObjectsOfType<MusicManager>();
-
-        if (objs.Length > 1)
-        {
+        if (objs.Length > 1) {
             Destroy(this.gameObject);
         }
-
         DontDestroyOnLoad(this.gameObject);
     }
 }

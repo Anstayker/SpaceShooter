ab0e8685d635811cddd58957a58dfd3fc8a835ad
Kevin Aguilar <aguilarkevin825@gmail.com>
2021-12-02 21:26:59 -0400

    Sound Effects

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour {
+
+    private GameObject _explosion;
+    public GameObject explosionPrefab;
     private void OnParticleCollision(GameObject other) {
-        Destroy(gameObject);
+       _explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+       Destroy(_explosion, 2);       
+       Destroy(gameObject);
+       
     }
     
 }
--- Assets/Scripts/PlayerShot.cs
+++ Assets/Scripts/PlayerShot.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 public class PlayerShot : MonoBehaviour {
     
     [SerializeField] ParticleSystem[] particleSystems;
+    private AudioSource _audioSource;
+    void Start() {
+        foreach (ParticleSystem gun in particleSystems)
+        {            
+            _audioSource = gun.GetComponent<AudioSource>();
+        }
+        
+    }
 
     public void ProcessShoot(bool isShooting) {
         foreach (ParticleSystem gun in particleSystems) {
-            if (isShooting)
+            if (isShooting) {
                 gun.Play();
-            else
+                _audioSource.loop = true;
+                _audioSource.Play();
+            }
+                
+            else{
                 gun.Stop();
+                _audioSource.loop = false;
+            }
         }
     }
 

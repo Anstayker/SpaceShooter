53d390a5b9c02aa548d3191d6a3897ea342465a7
elizabethcusiumss <ely.ecl88@gmail.com>
2021-12-24 11:10:35 -0400

    puntaje funcionalidad completa

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,16 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
     private GameObject _explosion;
     [SerializeField] private float enemyHealth = 1.0f;
     public GameObject explosionPrefab;
+    public PlayerMovement player;
+
+    private void Start() {
+        player = FindObjectOfType<PlayerMovement>();
+    }
   
     private void OnParticleCollision(GameObject other) {
       enemyHealth--;
-      contador = contador + 1 ;
-      puntuacion.text= "Score: " +contador;
+	player.contador=player.contador+1;
+	player.score.text = "Score: " + player.contador;
       if(enemyHealth <= 0) {
           _explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
           Destroy(_explosion, 2);       
